5f22fef15dab13a407cf612747152d6c13520634
AG89Z <agiachino@zmorph3d.com>
2020-05-27 23:40:55 +0200

    Setup form component

---
+++ form-component/rollup.config.js
@@ -0,0 +1,37 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+
+const dist = 'dist';
+const bundleName = 'bundle'
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default [
+  {
+    input: "src/main.js",
+    output: [
+      { file: `${dist}/${bundleName}.cjs.js`, format: "cjs" },
+      { file: `${dist}/${bundleName}.esm.js`, format: "esm" },
+      {
+        file: `${dist}/${bundleName}.umd.js`,
+        format: "umd",
+        name: "UrlShortenerForm",
+        globals: {
+          react: "React",
+        },
+      },
+    ],
+    plugins: [
+      babel({
+        exclude: ["node_modules/**"],
+        babelHelpers: "bundled", //TODO change to runtime
+      }),
+      resolve(),
+      commonjs(),
+      production && terser(),
+    ],
+    external: ["react"],
+  },
+];
---
+++ form-component/src/main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UrlShortenerForm() {
+  return <div>Hello form</div>;
+}
+
+export default UrlShortenerForm;
