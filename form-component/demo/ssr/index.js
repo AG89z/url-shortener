dcf04a7a36df8c87f84e6adacad931b48aacb41d
AG89Z <agiachino@zmorph3d.com>
2020-05-28 00:08:56 +0200

    Setup example folders

---
+++ form-component/demo/ssr/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const React = require("react");
+const ReactDOM = require("react-dom/server");
+
+const UrlShortenerForm = require("url-shortener-form");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const form = React.createElement(UrlShortenerForm);
+  const html = ReactDOM.renderToString(form);
+
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+      <body>
+        ${html}
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000, () => console.log("http://localhost:3000"));
