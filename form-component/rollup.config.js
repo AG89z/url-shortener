2270cad99962fb03195b33dc34105dc6010aa508
AG89Z <agiachino@zmorph3d.com>
2020-05-28 04:40:53 +0200

    Form css

--- form-component/rollup.config.js
+++ form-component/rollup.config.js
@@ -2,9 +2,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
+import { string } from "rollup-plugin-string";
 
-const dist = 'dist';
-const bundleName = 'bundle'
+const dist = "dist";
+const bundleName = "bundle";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -30,6 +31,7 @@ export default [
       }),
       resolve(),
       commonjs(),
+      string({ include: "**/*.css" }),
       production && terser(),
     ],
     external: ["react"],
--- form-component/src/main.js
+++ form-component/src/main.js
@@ -1,23 +1,70 @@
 import React, { useState } from "react";
+import style from "./style.css";
 
 function UrlShortenerForm() {
   const [state, setState] = useState({
     url: "",
-    expiry: null,
-    password: null,
+    expiry: "",
+    password: "",
   });
 
-  // const setUrl = (url) => setState((prevState) => ({ ...prevState, url }));
-  // const setExpiry = (expiry) => setState((prevState) => ({ ...prevState, expiry }));
-  // const setPassword = (password) => setState((prevState) => ({ ...prevState, password }));
+  const setUrl = (url) => setState((prevState) => ({ ...prevState, url }));
+  const setExpiry = (expiry) => setState((prevState) => ({ ...prevState, expiry }));
+  const setPassword = (password) => setState((prevState) => ({ ...prevState, password }));
 
   return (
-    <div>
-      <form>
-        SOme text
-
-      </form>
-    </div>
+    <>
+      <style>{style}</style>
+      <div className="urlFormShortener">
+        <h3 id="ufs-title">URL shortener</h3>
+        <form id="ufs-form">
+          <div id="ufs-url-area">
+            <input
+              id="ufs-url"
+              name="url"
+              type="url"
+              value={state.url}
+              onChange={(e) => {
+                console.log(e.target.validity);
+                setUrl(e.target.value);
+              }}
+              required
+              placeholder="URL"
+            />
+            <input id="ufs-submit" type="submit" />
+          </div>
+          <div id="ufs-security-note">
+            <p>
+              Although very difficult, a malicious party could discover a short link and from there
+              get to the destination URL. If your URL contains sensible data, consider protecting it
+              with a password and/or and expiry date.
+            </p>
+          </div>
+          <div id="ufs-password-expiry-area">
+            <label>
+              Password
+              <input
+                id="ufs-password"
+                name="password"
+                type="password"
+                value={state.password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <label>
+              Expiry date
+              <input
+                id="ufs-expiry"
+                name="expiry"
+                type="date"
+                value={state.expiry}
+                onChange={(e) => setExpiry(e.target.value)}
+              />
+            </label>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
 
