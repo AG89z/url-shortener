eb96e716298a890cb57f780e84cdba01042f6e54
AG89Z <agiachino@zmorph3d.com>
2020-05-28 02:04:09 +0200

    Simple development environemt

--- form-component/src/main.js
+++ form-component/src/main.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UrlShortenerForm() {
-  return <div>Hello form</div>;
+  const [state, setState] = useState({
+    url: "",
+    expiry: null,
+    password: null,
+  });
+
+  // const setUrl = (url) => setState((prevState) => ({ ...prevState, url }));
+  // const setExpiry = (expiry) => setState((prevState) => ({ ...prevState, expiry }));
+  // const setPassword = (password) => setState((prevState) => ({ ...prevState, password }));
+
+  return (
+    <div>
+      <form>
+        SOme text
+
+      </form>
+    </div>
+  );
 }
 
 export default UrlShortenerForm;
