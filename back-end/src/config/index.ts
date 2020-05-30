efbb95b047a8870490a949e49d3654e1ec53b1e0
AG89Z <agiachino@zmorph3d.com>
2020-05-30 22:40:18 +0200

    Setup eslint

--- back-end/src/config/index.ts
+++ back-end/src/config/index.ts
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 const envFound = dotenv.config();
 
 if (envFound.error) {
-  //throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
 }
 
 export = Object.freeze({
-  PORT: process.env.PORT || "5001",
-  NODE_ENV: process.env.NODE_ENV || "development",
-  JWT_SECRET: process.env.JWT_SECRET || "very_secret",
-  ALLOWED_DESTINATIONS: process.env.ALLOWED_DESTINATIONS || ".*",
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET,
+  ALLOWED_DESTINATIONS: process.env.ALLOWED_DESTINATIONS,
 });
--- back-end/src/controllers/links.ts
+++ back-end/src/controllers/links.ts
@@ -1,4 +1,4 @@
-import { Router, Request } from "express";
+import { Router } from "express";
 
 import LinksService from "../services/links";
 import { wrapAsync } from "../utils/wrapAync";
