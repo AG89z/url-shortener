127a6a77881cadbd1a40c840b09f06ef99fc8d7e
AG89Z <agiachino@zmorph3d.com>
2020-06-08 17:56:45 +0200

    Remove API code

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -3,7 +3,6 @@ import config from "./config";
 import express, { Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 
-import linksController from "./controllers/links";
 import linksGatewayController from "./controllers/gateway";
 
 import morgan from "morgan";
@@ -17,26 +16,17 @@ app.use(morgan("tiny"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("api"));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-app.get("/apii", (req, res) => {
-  res.render("../src/views/api");
-});
-
-app.use(linksController);
 app.use(linksGatewayController);
+
 app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`App listening on port http://localhost:${PORT}`);
 });
-
-function logError(err: Record<string, unknown>) {
-  console.log("Error caught by the default handler:");
-  console.log(err);
-}
 
 function errorHandler(
   err: Record<string, unknown>,
@@ -44,8 +34,8 @@ function errorHandler(
   res: Response,
   next: NextFunction
 ) {
-  logError(err);
-
+  console.log("Error caught by the default handler:");
+  console.log(err);
   const name = typeof err.name === "string" ? err.name : "UNKNOWN_ERROR";
   const status = typeof err.status === "number" ? err.status : 500;
   const message =
